9fc7be16575b339bccab9964ed170d62276fe554
DeCorrean Davis <112592380+DeCorreanD@users.noreply.github.com>
2023-07-27 07:23:11 -0500

    Making Cart

---
+++ utils/data/orderData.js
@@ -0,0 +1,62 @@
+import { clientCredentials } from '../client';
+
+const getOrder = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/order`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `${id}`,
+    },
+  })
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+}); const getSingleOrder = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/Order/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createOrder = (order) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/order`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(order),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+}); const updateOrder = (order) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/order/${order.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(order),
+  })
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteOrder = (id) => new Promise((resolve, reject) => {
+  fetch(`${clientCredentials.databaseURL}/order/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then(resolve)
+    .catch(reject);
+});
+
+export {
+  getOrder, deleteOrder, createOrder, updateOrder, getSingleOrder,
+};
